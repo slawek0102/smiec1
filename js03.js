b1e671eb78f7d05c55535e4219713afab36bfe49
slawek0102 <grzebyk.slawomir@gmail.com>
2016-11-02 14:42:40 +0100

    kolory

--- js03.js
+++ js03.js
@@ -1,17 +1,52 @@
 /**
  * Created by lkmk on 2016-11-02.
  */
-document.querySelector('button').addEventListener('click', function () {
-    var wartosc = document.querySelector('input').value;
-    if (wartosc === '') {
-        alert('Wypelnij pole');
-        return;
+
+
+document.querySelector('select').addEventListener('change', function () {
+
+    var value = document.querySelector('select').value;
+    var kolor = '#fff';
+
+    console.log(kolor);
+
+    switch (value) {
+        case '1':
+            kolor = '#f00';
+            break;
+        case '2':
+            kolor = '#0f0';
+            break;
+        case '3':
+            kolor = '#00f';
+            break;
+        default:
+            kolor = '#fff';
     }
-    var liczba = Number(wartosc);
-    if (isNaN(liczba)) {
-        console.log('konwerja nie poszla');
-        return;
-    }
-    console.log((typeof (liczba)));
+
+     // document.body.style = 'background-color:' + kolor;
+
+    document.body.style.backgroundColor = kolor;
+    console.log(value);
+    console.log(kolor);
+
 });
-//# sourceMappingURL=js03.js.map
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
