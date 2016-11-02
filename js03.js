f0824573adc6b6e3a6a03abc2f9e247ae242398d
Student <Student>
2016-11-02 12:34:27 +0100

    first commit

---
+++ js03.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lkmk on 2016-11-02.
+ */
+document.querySelector('button').addEventListener('click', function () {
+    var wartosc = document.querySelector('input').value;
+    alert(wartosc);
+});
+//# sourceMappingURL=js03.js.map
---
+++ js03.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by lkmk on 2016-11-02.
+ */
+
+
+document.querySelector('button').addEventListener('click', function () {
+
+    const wartosc = document.querySelector('input').value;
+
+   alert(wartosc);
+});
