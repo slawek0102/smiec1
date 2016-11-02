d701d910a17995699357360a7619181abdbb0379
slawek0102 <grzebyk.slawomir@gmail.com>
2016-11-02 13:23:15 +0100

    branch1

--- js03.js
+++ js03.js
@@ -3,6 +3,15 @@
  */
 document.querySelector('button').addEventListener('click', function () {
     var wartosc = document.querySelector('input').value;
-    alert(wartosc);
+    if (wartosc === '') {
+        alert('Wypelnij pole');
+        return;
+    }
+    var liczba = Number(wartosc);
+    if (isNaN(liczba)) {
+        console.log('konwerja nie poszla');
+        return;
+    }
+    console.log((typeof (liczba)));
 });
 //# sourceMappingURL=js03.js.map
--- js03.ts
+++ js03.ts
@@ -7,5 +7,17 @@ document.querySelector('button').addEventListener('click', function () {
 
     const wartosc = document.querySelector('input').value;
 
-   alert(wartosc);
+    if (wartosc === '') {
+        alert('Wypelnij pole');
+        return;
+    }
+
+    let liczba = Number(wartosc);
+
+    if (isNaN(liczba)){
+        console.log('konwerja nie poszla');
+        return;
+    }
+
+   console.log((typeof (liczba)));
 });
